8caf6d2ec0df34e7887558897d1e21f93c4524cd
dawidsadowski <dsadowski@craftware.biz>
2021-12-02 12:53:46 +0100

    Initial commit

---
+++ src/main/java/com/example/ppkwuzadanie5/PpkwuZadanie5Application.java
@@ -0,0 +1,13 @@
+package com.example.ppkwuzadanie5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuZadanie5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkwuZadanie5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ppkwuzadanie5/PpkwuZadanie5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppkwuzadanie5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpkwuZadanie5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
