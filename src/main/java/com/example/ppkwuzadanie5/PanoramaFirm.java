9b7beb08b1d0f3038068dcdddb77e552cd829235
dawidsadowski <dsadowski@craftware.biz>
2021-12-02 15:26:43 +0100

    Implement vCard generation

--- src/main/java/com/example/ppkwuzadanie5/PanoramaFirm.java
+++ src/main/java/com/example/ppkwuzadanie5/PanoramaFirm.java
@@ -1,5 +1,9 @@
 package com.example.ppkwuzadanie5;
 
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
+import ezvcard.VCardVersion;
+import ezvcard.property.Address;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -63,6 +67,9 @@ public class PanoramaFirm {
                 catch(Exception ignored) {}
             } catch(Exception ignored) {}
 
+            String params = String.format("name=%s&telephone=%s&email=%s&website=%s&street=%s&postalCode=%s&city=%s",
+                    name, telephone, email, website, street, postalCode, city).replaceAll(" ", "%20");
+
             html += String.format("<div>\n" +
                     "    <p><b>%s</b></p>\n" +
                     "    <p><img src=\"%s\" alt=\"logo firmy\"></img>\n" +
@@ -73,23 +80,31 @@ public class PanoramaFirm {
                     "    <p>%s</p>\n" +
                     "    <p>%s</p>\n" +
                     "    <p>%s</p>\n" +
-                    "    <p><button>Generuj VCard</button></p>\n" +
+                    "    <p>" +
+                    "<a href=/api/vcard/?%s>" +
+                    "<button>Generuj vCard</button></a></p>\n" +
                     "  </div><br/><br/>",
-                    name, image, telephone, email, website, street, postalCode, city);
+                    name, image, telephone, email, website, street, postalCode, city, params);
         }
 
         html += "</body></html>";
-
         return html;
-
-        //return String.format("Wyniki wyszukiwania dla frazy <b>%s</b><br/>%s:", query, "");
     }
 
-    public static String getVcard(String name, String image, String telephone, String email, String website, String street, String postalCode, String city) {
-        return "BEGIN:VCARD\r\n" +
-            "VERSION:4.0\r\n" +
-            "N:Doe;Jonathan;;Mr;\r\n" +
-            "FN:" + name + "\r\n" +
-            "END:VCARD\r\n";
+    public static String getVcard(String name, String telephone, String email, String website, String street, String postalCode, String city) {
+        VCard vcard = new VCard();
+
+        Address address = new Address();
+        address.setPostalCode(postalCode);
+        address.setStreetAddress(street);
+        address.setRegion(city);
+
+        vcard.setFormattedName(name);
+        vcard.addTelephoneNumber(telephone);
+        vcard.addEmail(email);
+        vcard.addUrl(website);
+        vcard.addAddress(address);
+
+        return Ezvcard.write(vcard).version(VCardVersion.V4_0).go();
     }
 }
--- src/main/java/com/example/ppkwuzadanie5/controller/CompanyVCardController.java
+++ src/main/java/com/example/ppkwuzadanie5/controller/CompanyVCardController.java
@@ -14,15 +14,14 @@ public class CompanyVCardController {
         return PanoramaFirm.getResults(query);
     }
 
-    @GetMapping(value = "/vcard/{query}", produces = {"text/vcard"})
+    @GetMapping(value = "/vcard/", produces = {"text/vcard"})
     public String getVcard(@RequestParam String name,
-                           @RequestParam String image,
                            @RequestParam String telephone,
                            @RequestParam String email,
                            @RequestParam String website,
                            @RequestParam String street,
                            @RequestParam String postalCode,
                            @RequestParam String city) {
-        return PanoramaFirm.getVcard(name, image, telephone, email, website, street, postalCode, city);
+        return PanoramaFirm.getVcard(name, telephone, email, website, street, postalCode, city);
     }
 }
