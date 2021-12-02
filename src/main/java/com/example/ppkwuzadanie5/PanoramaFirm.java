861e564252d02ff05ef9c03deca7d767a4dc385d
dawidsadowski <dsadowski@craftware.biz>
2021-12-02 14:34:59 +0100

    Add showing basic contact cards on endpoint query

--- src/main/java/com/example/ppkwuzadanie5/PanoramaFirm.java
+++ src/main/java/com/example/ppkwuzadanie5/PanoramaFirm.java
@@ -1,7 +1,89 @@
 package com.example.ppkwuzadanie5;
 
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class PanoramaFirm {
-    public static String getResults(String query) {
-        return String.format("Wyniki wyszukiwania dla frazy <b>%s</b>:", query);
+
+    static String html = "<!doctype html>\n" +
+            "<html>\n" +
+            "<head>\n" +
+            "  <title>VCard dla Panoramy Firm</title>" +
+            "  <meta charset=\"utf-8\">\n" +
+            "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n" +
+            "</head>\n" +
+            "<body>\n";
+//            "</body>\n" +
+//            "</html>";
+
+    public static String getResults(String query) throws IOException {
+        html += "<div>Wyniki wyszukiwania dla frazy <b>" + query + "</b>:</div>";
+
+        Document doc = Jsoup.connect("https://panoramafirm.pl/" + query).get();
+
+        Elements elements = doc.select("script[type=\"application/ld+json\"]");
+        elements.remove(elements.last());
+
+        String name = "";
+        String image = "";
+        String telephone = "";
+        String email = "";
+        String website = "";
+
+        String street = "";
+        String city = "";
+        String postalCode = "";
+
+        for(Element element : elements) {
+            String jsonString = element.html();
+            JSONObject jsonObject = new JSONObject(jsonString);
+
+            try {name = jsonObject.getString("name"); }
+            catch(Exception ignored) {}
+            try {image = jsonObject.getString("image"); }
+            catch(Exception ignored) {}
+            try {telephone = jsonObject.getString("telephone"); }
+            catch(Exception ignored) {}
+            try {email = jsonObject.getString("email"); }
+            catch(Exception ignored) {}
+            try {website = jsonObject.getString("sameAs"); }
+            catch(Exception ignored) {}
+
+            try {
+                JSONObject address = jsonObject.getJSONObject("address");
+
+                try {street = address.getString("streetAddress"); }
+                catch(Exception ignored) {}
+                try {city = address.getString("addressLocality"); }
+                catch(Exception ignored) {}
+                try {postalCode = address.getString("postalCode"); }
+                catch(Exception ignored) {}
+            } catch(Exception ignored) {}
+
+            html += String.format("<div>\n" +
+                    "    <p><b>%s</b></p>\n" +
+                    "    <p><img src=\"%s\" alt=\"logo firmy\"></img>\n" +
+                    "    <p>Telefon: %s</p>\n" +
+                    "    <p>Adres e-mail: %s</p>\n" +
+                    "    <p>Strona internetowa: %s</p>\n" +
+                    "  \n" +
+                    "    <p>%s</p>\n" +
+                    "    <p>%s</p>\n" +
+                    "    <p>%s</p>\n" +
+                    "    <p><button>Generuj VCard</button></p>\n" +
+                    "  </div><br/><br/>",
+                    name, image, telephone, email, website, street, postalCode, city);
+        }
+
+        html += "</body></html>";
+
+        return html;
+
+        //return String.format("Wyniki wyszukiwania dla frazy <b>%s</b><br/>%s:", query, "");
     }
 }
--- src/main/java/com/example/ppkwuzadanie5/controller/CompanyVCardController.java
+++ src/main/java/com/example/ppkwuzadanie5/controller/CompanyVCardController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api")
 public class CompanyVCardController {
 
     @GetMapping("/{query}")
-    public String getCompanyList(@PathVariable String query) {
+    public String getCompanyList(@PathVariable String query) throws IOException {
         return PanoramaFirm.getResults(query);
     }
 }
